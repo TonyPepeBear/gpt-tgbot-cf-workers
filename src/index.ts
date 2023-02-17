7eeec3d77fe4684e70c32eeae4b5e5f60937daae
TonyPepe <tonybear55665566@gmail.com>
2023-02-17 22:46:08 +0800

    Update src/index.ts to add Telegram webhook and parse URL to respond with "Hello world!"

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
+  MY_KV_NAMESPACE: KVNamespace;
   //
   // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
   // MY_DURABLE_OBJECT: DurableObjectNamespace;
@@ -28,6 +18,18 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response("Hello World!");
+     const tg_token = "123" // env.MY_KV_NAMESPACE.get("tg_token");
+
+    // parse url
+    const url = new URL(request.url);
+
+    // telegram webhook
+    if (url.pathname === `/bot${tg_token}`) {
+      
+      return new Response("Hello world!");
+    }
+
+    // ignore other requests
+    return new Response("Not found", { status: 404 });
   },
 };
