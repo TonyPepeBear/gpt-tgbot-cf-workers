eaa6ea3373de35e0bbfce4d0e1b452316eafe6a4
TonyPepe <tonybear55665566@gmail.com>
2023-02-18 02:36:04 +0800

    Update code to use environment variable for Telegram token and add logic for sending response message to Telegram sender

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Update } from "@grammyjs/types";
 export interface Env {
-  MY_KV_NAMESPACE: KVNamespace;
+  TG_TOKEN: string;
 }
 
 export default {
@@ -9,18 +9,36 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const tg_token = "123"; // env.MY_KV_NAMESPACE.get("tg_token");
     // parse url
     const url = new URL(request.url);
 
     // telegram webhook
-    if (request.method === "POST" && url.pathname === `/bot${tg_token}`) {
+    if (request.method === "POST" && url.pathname === `/bot${env.TG_TOKEN}`) {
       let update: Update = JSON.parse(await request.text());
+      console.log(update.message?.text);
+      const echo = fetch(createSentMessageUrl(env.TG_TOKEN), {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: update.message?.chat.id,
+          text: update.message?.text + "\nfrom worker",
+        }),
+      });
+      ctx.waitUntil(echo);
       return new Response(update.message?.chat.id! + update.message?.text!);
     }
     // ignore other requests
-    return new Response("404 - I am a telegram bot with webhook.", {
-      status: 404,
-    });
+    return new Response(
+      "404 - I am a telegram bot with webhook." + "\n" + request.url,
+      {
+        status: 404,
+      }
+    );
   },
 };
+
+// create sentMessage url
+const createSentMessageUrl = (token: string) =>
+  `https://api.telegram.org/bot${token}/sendMessage`;
