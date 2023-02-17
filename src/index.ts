287c5fb7ff47d24c154e943509e3e718b93cc71a
TonyPepe <tonybear55665566@gmail.com>
2023-02-17 23:05:50 +0800

    Remove unused KV, Durable Object, R2, and Service bindings and update telegram webhook response.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
   MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
 }
 
 export default {
@@ -18,18 +8,17 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-     const tg_token = "123" // env.MY_KV_NAMESPACE.get("tg_token");
+    const tg_token = "123"; // env.MY_KV_NAMESPACE.get("tg_token");
 
     // parse url
     const url = new URL(request.url);
 
     // telegram webhook
-    if (url.pathname === `/bot${tg_token}`) {
-      
+    if (request.method === "POST" && url.pathname === `/bot${tg_token}`) {
       return new Response("Hello world!");
     }
 
     // ignore other requests
-    return new Response("Not found", { status: 404 });
+    return new Response("404 - I am a telegram bot with webhook.", { status: 404 });
   },
 };
